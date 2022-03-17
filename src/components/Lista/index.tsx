f333cefc071eead30119bf4b1c420c0654d8a8cd
Kaue Oliveira <ksilverio@arpiatecnologia.com.br>
2022-03-17 13:22:12 -0300

    added sass and styles

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./List.module.scss"
 
 function Lista(){
     const tarefas = [{
@@ -14,11 +15,11 @@ function Lista(){
         tempo: '03:00:00'
     }]
     return(
-        <aside>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((tarefa,index) => (
-                    <li key={index}>
+                    <li key={index} className={style.item}>
                         <h3>{tarefa.item}</h3>
                         <p>{tarefa.tempo}</p>
                     </li>
